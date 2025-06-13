37905ef9ef8246fc5d8d042494bd60d8d99ea41f
kisnaXD <krishnagera93@gmail.com>
2025-06-14 00:41:25 +0530

    Navbar done almost

--- i-fear-i-missed-out/src/Navbar/Navbar.jsx
+++ i-fear-i-missed-out/src/Navbar/Navbar.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState, useRef } from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <div className="navbar-container">
-            
-        </div>
-    )
-}
+  const [blobPosition, setBlobPosition] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
+  const elementsRef = useRef(null);
 
-export default Navbar
+  const handleMouseMove = (e) => {
+    if (!elementsRef.current) return;
+
+    const rect = elementsRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const blobSize = 50;
+    const clampedX = Math.max(blobSize / 2, Math.min(x, rect.width - blobSize / 2));
+    const clampedY = Math.max(blobSize / 2, Math.min(y, rect.height - blobSize / 2));
+
+    setBlobPosition({ x: clampedX, y: clampedY });
+    console.log(`Mouse move: x=${clampedX}, y=${clampedY}, navbar-elements: width=${rect.width}, height=${rect.height}`);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+    console.log('Mouse entered navbar-elements');
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+    console.log('Mouse left navbar-elements');
+  };
+
+  return (
+    <div className="navbar-container">
+      <div className="navbar-logo">
+        Missed <br /> Out?
+      </div>
+      <div
+        className="navbar-elements"
+        ref={elementsRef}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <a href="">Home</a>
+        <a href="">About Us</a>
+        <a href="">Contact Us</a>
+        <a href="">Other Calculators</a>
+        {isHovering && (
+          <div
+            className="navbar-blob"
+            style={{
+              left: `${blobPosition.x - 25}px`,
+              top: `${blobPosition.y - 25}px`,
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
